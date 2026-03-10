f3b5d9acd3f966811c3b489ef55a0906885514eb
VITA <tysichka11@gmail.com>
2026-03-10 05:40:48 +0100

    homework2

---
+++ hw3.3.py
@@ -0,0 +1,24 @@
+number = [1, 2, 3, 4 ,5, 6]
+middle = (len(number) + 1) // 2
+result = [number[:middle], number[middle:]]
+print(result)
+
+number = [1, 2, 3]
+middle = (len(number) + 1) // 2
+result = [number[:middle], number[middle:]]
+print(result)
+
+number = [1, 2, 3, 4 ,5,]
+middle = (len(number) + 1) // 2
+result = [number[:middle], number[middle:]]
+print(result)
+
+number = [1,]
+middle = (len(number) + 1) // 2
+result = [number[:middle], number[middle:]]
+print(result)
+
+number = []
+middle = (len(number) + 1) // 2
+result = [number[:middle], number[middle:]]
+print(result)

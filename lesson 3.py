71121d3077e5a1eb3d186a7a10357182f615fda3
VITA <tysichka11@gmail.com>
2026-03-11 17:04:00 +0100

    homework2

--- lesson 3.py
+++ lesson 3.py
@@ -42,6 +42,77 @@ else:
     #ввести рейтинг фільму : якщо рейтинг дорівнює 5 або 4 - ок, інакще - погано
     # film_rating = int(input("Enter film rating: ")
 
+number_b = 30
+#v1
+#if number_b <5:
+#   number_a = 10
+#else:
+#    number_a=20
+#v2
+number_a = 10 if number_b < 5 else 20
+print(number_a)
+
+######
+number_b =30
+if number_b <5
+    if number_b %2 == 0:
+        number_a = 10
+    else:number_a = 15
+else:
+    number_a = 20
+ number_a = 10 if number_b % 2 == 0 else 15 if number_b < 5 else 20
+ print(number_a)
+
+
+
+ ###############################
+лист список
+ #list
+ #number =[]
+ #numbers1 = list()
+ #print(type(numbers))
+ #print(type(numbers_1))
+ #
+ #numbers = [1, 3, 25, 7, 2, 7]
+ ###
+ ### print(numbers)
+ ###
+ ### [] -> індексатори
+   #### ІНДЕКС ЦЕ ПОРЯДКОВИЙ НОМЕР ЕЛЕМЕНТА
+
+
+numbers[1] = 11111
+print(numbers)
+
+print(len(numbers))
+print(numbers[-1])
+#
+#print(numbers[len(numbers)]) # [6] -> error
+
+###
+values =["one", 12, 12.4, True]
+print(values)
+
+#
+nums:list[int] = [1,3] * 5
+print(nums)
+ #
+ # slices
+ numbers = [1, 3, 25, 7, 2, 7]
+ print(numbers[:])
+ print(numbers[1:])
+ print(numbers[1:5])
+ print(numbers[:5])
+ print (numbers[1:5:2])
+print(numbers[::-1])
+print(numbers[5::-1])
+print(numbers[5:0:-1])
+
+##
+users = ()
+
+
+
 
 
 

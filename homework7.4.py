e8062a049e41c903e0a111441f05d6b3724c8090
VITA <tysichka11@gmail.com>
2026-03-24 16:56:08 +0100

    homework2

---
+++ homework7.4.py
@@ -0,0 +1,5 @@
+def common_elements():
+    list_3 = [x for x in range(100) if x % 3 == 0]
+    list_5 = [x for x in range(100) if x % 5 == 0]
+    return set(list_3) & set(list_5)
+print(common_elements())

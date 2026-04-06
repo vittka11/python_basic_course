ecd358517a6b1857277376bd7b1e7e2c9b27cd59
VITA <tysichka11@gmail.com>
2026-04-07 01:43:36 +0200

    homework2

---
+++ homework 10.3.py
@@ -0,0 +1,7 @@
+def is_even(digit):
+    """Chek if a number is even"""
+    return digit % 2 == 0
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
